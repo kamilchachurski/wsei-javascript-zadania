c118904d413f53fda7d5b110d5633faee7d38a3b
Kamil Chachurski <kamil.chachurski@microsoft.wsei.edu.pl>
2020-01-24 11:30:46 +0100

    Done exercises: 13.0, 13.1, 13.2, 13.3, 13.4, 13.5

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie01.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.button').addEventListener('click', () => {
+        let li = document.createElement('li');
+
+        li.innerText = document.querySelector('.menu').children.length;
+
+        document.querySelector('.menu').appendChild(li);
+    });
+});
